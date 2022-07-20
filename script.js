3d6965e623025783998df26c902d266b68eff610
Abhijeet Khan <abhijeetkhan28@gmail.com>
2022-07-20 17:37:01 +0530

    Change in script

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full Stack Developer", "Artsit", "Designer", "Freelancer"],
+        strings: ["Developer", "Artist", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Full Stack Developer", "Artist", "Designer", "Freelancer"],
+        strings: ["Developer", "Artist", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
